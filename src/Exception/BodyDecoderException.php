970e18b4caf9262db127c296c4f23c0d5391d91b
SerafimArts <nesk@xakep.ru>
2025-05-25 13:10:39 +0000

    Split http body decoder component

---
+++ src/Exception/BodyDecoderException.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boson\Component\Http\Body\Exception;
+
+class BodyDecoderException extends \RuntimeException {}
---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boson\Component\Http\Body\Tests;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+#[Group('boson-php/http-body-decoder')]
+abstract class TestCase extends BaseTestCase {}
